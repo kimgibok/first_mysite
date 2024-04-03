d2ff3e1b17f65f2aa1498079836e33127ca65b47
kimgibok <ruddjs33@naver.com>
2024-04-03 14:36:43 +0900

    base_site, debug

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,20 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    # fields = ["pub_date", "question_text"]
     fieldsets = [
-        ("Title", {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     
 admin.site.register(Question, QuestionAdmin)
-
-admin.site.register(Choice)
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -11,10 +12,14 @@ class Question(models.Model):
     # 매직매소드
     def __str__(self):
         return self.question_text
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description="Published recently?"
+    )
     def was_published_recently(self):
         return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
-    def length_question_text(self):
-        return len(self.question_text)
 
 
 class Choice(models.Model):
